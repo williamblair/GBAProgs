8325f039555401aaa63094100daadcf1547677f1
William Blair <blairw@sunyit.edu>
2018-05-25 19:13:51 +0000

    added bmp display in mode 4

---
+++ HelloBmp_Mode4/build/main.d
@@ -0,0 +1,10 @@
+main.o: /home/ubuntu/workspace/HelloBmp_Mode4/source/main.c \
+ /home/ubuntu/workspace/HelloBmp_Mode4/source/../../bjgba.h \
+ /home/ubuntu/workspace/HelloBmp_Mode4/source/../Untitled.raw.c \
+ /home/ubuntu/workspace/HelloBmp_Mode4/source/../Untitled.pal.c
+
+/home/ubuntu/workspace/HelloBmp_Mode4/source/../../bjgba.h:
+
+/home/ubuntu/workspace/HelloBmp_Mode4/source/../Untitled.raw.c:
+
+/home/ubuntu/workspace/HelloBmp_Mode4/source/../Untitled.pal.c:
