a05c0e12a945ee64727d831ae317f36c04e48a8d
William Blair <blairw@sunyit.edu>
2018-05-25 18:58:41 +0000

    added mode 4 hello world

---
+++ HelloMode4/build/main.d
@@ -0,0 +1,4 @@
+main.o: /home/ubuntu/workspace/HelloMode4/source/main.c \
+ /home/ubuntu/workspace/HelloMode4/source/../../bjgba.h
+
+/home/ubuntu/workspace/HelloMode4/source/../../bjgba.h:
--- bjgba.h
+++ bjgba.h
@@ -1,11 +1,13 @@
 /* Defines and such */
 
 // Registers
-#define REG_VIDBUF  0x6000000
 #define REG_DISPCNT 0x4000000
+#define REG_PALETTE 0x5000000
+#define REG_VIDBUF  0x6000000
 
 // Video modes
 #define MODE_3 0x3
+#define MODE_4 0x4
 
 // Backgrounds
 #define BG2_ENABLE 0x400
@@ -19,5 +21,30 @@
 
 // Packs 0-31 RGB color into 16bit
 #define RGB(r,g,b) \
-    (r + (g<<5) + (b<<10))
+    ((r) + ((g)<<5) + ((b)<<10))
+    
+// draw a pixel in mode 4
+static inline void drawPixel4(int x, int y, int color)
+{
+    unsigned short *vidBuf = (unsigned short *)REG_VIDBUF;
+    
+    // read the existing value, accessing divided by two
+    // since two pixels are stuffed into a single unsigned short
+    unsigned short offset = (y*240+x) >> 1;
+    unsigned short pixel = vidBuf[offset];
+    
+    // determine if the pixel is odd or even
+    if(x & 1)
+    {
+        // if odd, copy the color to the upper portion of the num
+        vidBuf[offset] = (pixel & 0xFF00) + color;
+    }
+    else
+    {
+        // if even, copy the color to the lower portion of the num
+        vidBuf[offset] = (pixel & 0x00FF) + (color << 8);
+    }
+    
+    return;
+}
     
