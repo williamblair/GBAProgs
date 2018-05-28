41ac637705692d33401797aec49302db3b91f730
williamblair <blairw@sunyit.edu>
2018-05-28 15:45:38 -0400

    added missing files

---
+++ DrawText_Mode3/build/main.d
@@ -0,0 +1,7 @@
+main.o: C:/Users/bj/programming/gba/GBAProgs/DrawText_Mode3/source/main.c \
+ c:\users\bj\programming\gba\gbaprogs\bjgba.h \
+ c:\users\bj\programming\gba\gbaprogs\bjfont.h
+
+c:\users\bj\programming\gba\gbaprogs\bjgba.h:
+
+c:\users\bj\programming\gba\gbaprogs\bjfont.h:
